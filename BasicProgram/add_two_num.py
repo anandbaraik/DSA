8151ba208497dce7ee3ca6a2cde0b6420a2c0124
anandbaraik <anandbaraik2014@gmail.com>
2021-07-31 21:31:33 +0530

    Wrote basic program

---
+++ BasicProgram/add_two_num.py
@@ -0,0 +1,5 @@
+# Get number from user and add it & display to user
+num1 = input("Enter first number: ")
+num2 =  input("Enter second number: ")
+sum = int(num1) + int(num2)
+print("Sum of {num1} & {num2} is {sum}.".format(num1=num1, num2=num2, sum=sum))
