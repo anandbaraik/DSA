eb9d55f2bce9ae1185c8d101c6b0cf1a1ffc1c9a
Anand Baraik <anandbaraik2014@gmail.com>
2022-10-07 23:06:29 +0530

    sasas
    asasas

--- BasicProgram/add_two_num.py
+++ BasicProgram/add_two_num.py
@@ -3,3 +3,5 @@ num1 = input("Enter first number: ")
 num2 =  input("Enter second number: ")
 sum = int(num1) + int(num2)
 print("Sum of {num1} & {num2} is {sum}.".format(num1=num1, num2=num2, sum=sum))
+
+test test test
