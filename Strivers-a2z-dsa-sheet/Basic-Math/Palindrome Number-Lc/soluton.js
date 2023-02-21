c2dd78d1cc20607b00347feb09e9c7e55f723406
anandbaraik <anandbaraik2014@gmail.com>
2023-02-21 21:53:41 +0530

    palindrome num code refacored

--- Strivers-a2z-dsa-sheet/Basic-Math/Palindrome Number-Lc/soluton.js
+++ Strivers-a2z-dsa-sheet/Basic-Math/Palindrome Number-Lc/soluton.js
@@ -4,17 +4,21 @@
  */
 
 const reverseNumber = function (num) {
-    let reverseNum = 0;
-    while(num > 0) {
-      let reminder = num % 10;
-      reverseNum = (reverseNum * 10) + reminder;
-      num = Math.trunc(num/10);
-    }
-    return reverseNum;
+  let reverseNum = 0;
+  while(num > 0) {
+    let reminder = num % 10;
+    reverseNum = (reverseNum * 10) + reminder;
+    num = Math.trunc(num/10);
   }
+  return reverseNum;
+}
 
-  var isPalindrome = function(x) {
-      if(x < 0) return false;
-      let reversedNum = reverseNumber(x);
-      return reversedNum === x;
-  };
+var isPalindrome = function(x) {
+    const MIN_VALUE = Math.pow(-2,31);
+    const MAX_VALUE = Math.pow(2,31)-1;
+    if(x >= MIN_VALUE && x <= MAX_VALUE ) {
+        let reversedNum = reverseNumber(x);
+        return reversedNum === x;
+    }
+    return false;
+};
